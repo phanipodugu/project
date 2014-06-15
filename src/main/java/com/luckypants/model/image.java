8e9766ede69ffaff6eb86ea288182f545a40da52
Phani <p.phani41@gmail.com>
2014-06-15 12:29:42 -0500

    project

---
+++ src/main/java/com/luckypants/model/image.java
@@ -0,0 +1,22 @@
+package com.luckypants.model;
+
+public class image {
+
+	
+	private String imagename;
+
+	/**
+	 * @return the imagename
+	 */
+	public String getImagename() {
+		return imagename;
+	}
+
+	/**
+	 * @param imagename the imagename to set
+	 */
+	public void setImagename(String imagename) {
+		this.imagename = imagename;
+	}
+	
+}
